08a85d8ff849c30e86bc791f9ef802b34bec94c0
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-06-21 10:18:36 +0200

    Update NnhiddenlayerForProPGroupsLlmodelsArray.h

--- NnhiddenlayerForProPGroupsLlmodelsArray.h
+++ NnhiddenlayerForProPGroupsLlmodelsArray.h
@@ -1,1 +1,4 @@
-
+//the scope of the virus will be large scale
+//low voltage systems a'd not a single system.
+//then, the os will check the real target.later.
+//as topologies are not interesting, we can ise llms.
